cd04a4548ae1d63536c5546d63b44b6d1a2219cd
Ingo Graebner <igrex@129.12.243.10.in-addr.arpa>
2018-09-24 18:01:05 +0200

    enable encryption for registration form and forgot password form

--- keycloak-password-encryption-js/login/resources/js/login-password-encryption.js
+++ keycloak-password-encryption-js/login/resources/js/login-password-encryption.js
@@ -1,9 +1,33 @@
 
 // On window load
 window.onload = function() {
-    const submitBtnElt = this.document.getElementById('kc-login');
-    const passwordElt = this.document.getElementById('password');
-    const authFormElt = this.document.getElementById("kc-form-login");
-    // browserFormPwdEncrypt come from the keycloak-password-encryption-js.js build file.
-    browserFormPwdEncrypt.registerPasswordSubmitEvent(passwordElt, null, null, submitBtnElt, authFormElt);
+
+    const authFormLoginElt = this.document.getElementById("kc-form-login");
+    const authFormUpdateElt = this.document.getElementById("kc-passwd-update-form");
+    const authFormRegisterElt = this.document.getElementById("kc-register-form");
+    
+    if(authFormLoginElt != null) {
+    	
+        const submitBtnLoginElt = this.document.getElementById('kc-login');
+        const passwordLoginElt = this.document.getElementById('password');
+
+        browserFormPwdEncrypt.registerPasswordSubmitEvent(passwordLoginElt, null, submitBtnLoginElt, authFormLoginElt);
+        
+    } else if(authFormUpdateElt != null) {
+    	
+        const passwordNewUpdateElt = this.document.getElementById('password-new');
+        const passwordConfirmUpdateElt = this.document.getElementById('password-confirm');
+        const submitBtnUpdateElt = this.document.getElementsByClassName('btn btn-primary btn-block btn-lg')[0];
+        
+        browserFormPwdEncrypt.registerPasswordSubmitEvent(passwordNewUpdateElt, passwordConfirmUpdateElt, submitBtnUpdateElt, authFormUpdateElt);
+    	
+    } else if (authFormRegisterElt != null) {
+
+        const passwordRegisterElt = this.document.getElementById('password');
+        const passwordConfirmRegisterElt = this.document.getElementById('password-confirm');
+        const submitBtnRegisterElt = this.document.getElementsByClassName('btn btn-primary btn-block btn-lg')[0];
+    	
+        browserFormPwdEncrypt.registerPasswordSubmitEvent(passwordRegisterElt, passwordConfirmRegisterElt, submitBtnRegisterElt, authFormRegisterElt);
+    	
+    }
 }
--- keycloak-password-encryption-js/login/src/browser-form-pwd-encryptor.js
+++ keycloak-password-encryption-js/login/src/browser-form-pwd-encryptor.js
@@ -2,33 +2,71 @@
 
 const jwksDownloader = require('./jwks-downloader')
 
-function registerPasswordSubmitEvent (passwordElt, confirmPwdElt1, confirmPwdElt2, submitBtnElt, authFormElt) {
+function registerPasswordSubmitEvent(passwordElt, passwordConfirmElt, submitBtnElt, authFormElt) {
     if(!passwordElt) {
         throw "No password elt provided"
     }
+
     submitBtnElt.onclick = function(e) {
-        // enhancedPwd with a 'timestamp' value (to prevent a replay attact)
-        const enhancedPwd = {
-            timestamp: new Date(),
-            pwd: passwordElt.value
+        // check if the passwords match 
+        if(validatePasswordConfirm(passwordElt, passwordConfirmElt)) {
+
+            // enhancedPwd with a 'timestamp' value (to prevent a replay attack)
+            const enhancedPwd = {
+                timestamp: new Date(),
+                pwd: passwordElt.value
+            }
+
+            jwksDownloader.keyPromise
+                .then(encKey => {
+                    return jwksDownloader.encryptPwd(enhancedPwd, encKey)
+                })
+                .then(encryptedPwd => {
+                    passwordElt.value = encryptedPwd;
+                    if(passwordConfirmElt != null)
+                    	passwordConfirmElt.value = encryptedPwd;
+                    // Submit the form when the password encryption is completed.
+                    authFormElt.submit();
+                })
+                .catch(error => {
+                    throw error;
+                })
+            ;
+
+            return false;
+        } else {
+            return false;
         }
-
-        jwksDownloader.keyPromise
-            .then(encKey => {
-                return jwksDownloader.encryptPwd(enhancedPwd, encKey)
-            })
-            .then(encryptedPwd => {
-                passwordElt.value = encryptedPwd;
-                // Submit the form when the password encryption is completed.
-                authFormElt.submit();
-            })
-            .catch(error => {
-                throw error;
-            })
-
-        return false;
     }
 }
+
+function validatePasswordConfirm(passwordElt, passwordConfirmElt) {
+    if(passwordConfirmElt == null)
+        return true;
+    if(passwordElt.value == passwordConfirmElt.value)
+        return true; 
+    
+    // display error 
+    var mismatchError = document.getElementById('mismatchError');
+    if(mismatchError == null) {
+        var divAlert = document.createElement('div');
+        divAlert.id = 'mismatchError';
+        divAlert.className = 'alert alert-error';
+        var spanAlert = document.createElement('span');
+        spanAlert.className = 'pficon pficon-error-circle-o';
+        divAlert.appendChild(spanAlert);
+        var spanText = document.createElement('span');
+        spanText.className = 'kc-feedback-text';
+        spanText.textContent = 'Password confirmation doesn\'t match.';
+        divAlert.appendChild(spanText);
+        
+        var contentWrapper = document.getElementById('kc-content-wrapper');
+        contentWrapper.parentElement.insertBefore(divAlert, contentWrapper);
+    }
+
+    return false;
+}
+
 module.exports = {
     registerPasswordSubmitEvent
 }
