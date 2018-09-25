6fdc69f862c60577648fdb21d82737b2dc58f78c
Ingo Graebner <igrex@146.1.243.10.in-addr.arpa>
2018-09-25 12:02:34 +0200

    log warn messages instead stacktrace

--- src/main/java/de/adorsys/keycloak/password/encryption/PasswordEncryptedCredentialProvider.java
+++ src/main/java/de/adorsys/keycloak/password/encryption/PasswordEncryptedCredentialProvider.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
+import org.jboss.logging.Logger;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.PasswordCredentialProvider;
 import org.keycloak.models.KeyManager.ActiveRsaKey;
@@ -19,60 +20,74 @@ import com.nimbusds.jose.crypto.RSADecrypter;
 import net.minidev.json.JSONObject;
 
 public class PasswordEncryptedCredentialProvider extends PasswordCredentialProvider {
+	private static final Logger logger = Logger.getLogger(PasswordEncryptedCredentialProvider.class);
+
 	public PasswordEncryptedCredentialProvider(KeycloakSession session) {
 		super(session);
 	}
 
 	@Override
 	public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
-		transformPassword(session, realm, (PasswordUserCredentialModel) input);
+		try {
+			transformPassword(session, realm, (PasswordUserCredentialModel) input);
+		} catch (IllegalStateException e) {
+			return false;
+		}
 		return super.updateCredential(realm, user, input);
-		
+
 	}
 
 	@Override
 	public boolean isValid(RealmModel realm, UserModel user, CredentialInput input) {
-		transformPassword(session, realm, (PasswordUserCredentialModel) input);
+		try {
+			transformPassword(session, realm, (PasswordUserCredentialModel) input);
+		} catch (IllegalStateException  e) {
+			return false;
+		}
 		return super.isValid(realm, user, input);
 	}
-	
-	private static final void transformPassword(KeycloakSession session, RealmModel realm, final PasswordUserCredentialModel input){
-        ActiveRsaKey rsaKey = session.keys().getActiveRsaKey(realm);
-        // read Password from input data
-        String passwordJWE = input.getValue();
+
+	private static final void transformPassword(KeycloakSession session, RealmModel realm,
+			final PasswordUserCredentialModel input) throws IllegalStateException {
+		ActiveRsaKey rsaKey = session.keys().getActiveRsaKey(realm);
+		// read Password from input data
+		String passwordJWE = input.getValue();
 
 		// Parse JWE
-        JWEObject jweObject;
-        try {
+		JWEObject jweObject;
+		try {
 			jweObject = JWEObject.parse(passwordJWE);
 		} catch (ParseException e) {
+			logger.warn(e.getMessage());
 			throw new IllegalStateException(e);
+
 		}
 
 		// decrypt password using keycloak private key
-        // Decrypt with private key
-        try {
+		// Decrypt with private key
+		try {
 			jweObject.decrypt(new RSADecrypter(rsaKey.getPrivateKey()));
 		} catch (JOSEException e) {
+			logger.warn(e.getMessage());
 			throw new IllegalStateException(e);
 		}
-        JSONObject jsonObject = jweObject.getPayload().toJSONObject();
-        String timestamp = (String)jsonObject.get("timestamp");
-        
-        // Validate timestap
-        // - Parse timestamp
-        // - make sure time is not far in the pass.
-        ZonedDateTime dateTime = ZonedDateTime.parse(timestamp);
-        ZonedDateTime now = ZonedDateTime.now();
-        if(ChronoUnit.MINUTES.between(dateTime, now) > 2) {
-        	throw new IllegalStateException("Timestamp is to far in the past.");
-        }
-        
-        String password = (String)jsonObject.get("pwd");
-        
-        // Set clreartext password in inputData
-        input.setValue(password);
+		JSONObject jsonObject = jweObject.getPayload().toJSONObject();
+		String timestamp = (String) jsonObject.get("timestamp");
+
+		// Validate timestap
+		// - Parse timestamp
+		// - make sure time is not far in the pass.
+		ZonedDateTime dateTime = ZonedDateTime.parse(timestamp);
+		ZonedDateTime now = ZonedDateTime.now();
+		if (ChronoUnit.MINUTES.between(dateTime, now) > 2) {
+			logger.warn("Timestamp is to far in the past.");
+			throw new IllegalStateException("Timestamp is to far in the past.");
+		}
+
+		String password = (String) jsonObject.get("pwd");
+
+		// Set clreartext password in inputData
+		input.setValue(password);
 	}
-	
 
 }
